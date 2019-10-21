1d80e6fe0019d9ad657252696056fb6777674796
muhamob <yusov.aa@phystech.edu>
2019-10-22 01:04:44 +0300

    Add e step calculation

---
+++ hw/practice_1/utils/BMML2_open_tests.py
@@ -0,0 +1,122 @@
+import sys
+import time
+import numpy as np
+from nose.tools import ok_, eq_
+from numpy.testing import assert_almost_equal
+from . import yusov as model
+
+
+def test_python3():
+    ok_(sys.version_info > (3, 3))
+
+
+def check_shape(**kwargs):
+    np.random.seed(42)
+    H, W, K = 10, 12, 20
+    h, w = 4, 5
+    max_iter = 1
+    X = np.random.rand(H, W, K)
+    F, B, s, A, LL = model.run_EM(X, h, w, max_iter=max_iter, **kwargs)
+    eq_(F.shape, (h, w))
+    eq_(B.shape, (H, W))
+    eq_(A.shape, (H-h+1, W-w+1))
+    eq_(np.array(LL).shape, (max_iter,))
+
+
+def generate_data(H, W, K, h, w, use_MAP=False, seed=42):
+    # explicit data with no noise
+    np.random.seed(seed)
+    X = np.zeros((H, W, K))
+    F = np.zeros((h, w))
+    B = np.random.rand(H, W)
+
+    coords = []
+    q = np.zeros((H-h+1, W-w+1, K))
+    for k in range(K):
+        x = np.random.randint(0, H-h+1)
+        y = np.random.randint(0, W-w+1)
+        coords.append((x, y))
+        X[:, :, k] = np.copy(B)
+        X[x:x+h, y:y+w, k] = F
+        q[x, y, k] = 1.
+
+    A = np.random.rand(H - h + 1, W - w + 1)
+    A /= A.sum()
+
+    if use_MAP:
+        q = np.array(coords).T
+
+    return X, F, B, A, q
+
+
+def check_e_step(use_MAP=False):
+    H, W, K = 4, 5, 2
+    h, w = 2, 3
+    s = 1e-1
+    X, F, B, A, q = \
+        generate_data(H, W, K, h, w, use_MAP=use_MAP)
+
+    pred_q = model.run_e_step(X, F, B, s, A, use_MAP=use_MAP)
+    if use_MAP:
+        assert_almost_equal(q, pred_q)
+    else:
+        assert_almost_equal(q, pred_q, 5)
+
+
+def check_m_step(use_MAP=False):
+    H, W, K = 7, 8, 2
+    h, w = 2, 3
+    X, F, B, A, q = generate_data(H, W, K, h, w, use_MAP=use_MAP)
+
+    pred_F, pred_B, pred_s, pred_A = \
+        model.run_m_step(X, q, h, w, use_MAP=use_MAP)
+
+    assert_almost_equal(F, pred_F)
+    assert_almost_equal(B, pred_B)
+
+
+def check_e_step_time(use_MAP=False):
+    H, W, K = 50, 100, 50
+    h, w = 40, 50
+    s = 0.1
+    X, F, B, A, q = generate_data(H, W, K, h, w)
+    t_start = time.perf_counter()
+    model.run_e_step(X, F, B, s, A, use_MAP=use_MAP)
+    computation_time = time.perf_counter() - t_start
+    assert computation_time < 1
+
+
+def check_m_step_time(use_MAP=False):
+    H, W, K = 50, 100, 50
+    h, w = 40, 50
+    X, F, B, A, q = generate_data(H, W, K, h, w, use_MAP=use_MAP)
+
+    t_start = time.perf_counter()
+    model.run_m_step(X, q, h, w, use_MAP=use_MAP)
+    computation_time = time.perf_counter() - t_start
+    assert computation_time < 1
+
+
+def test_output_shape():
+    check_shape()
+    check_shape(use_MAP=True)
+
+
+def test_e_step():
+    check_e_step()
+    check_e_step(use_MAP=True)
+
+
+def test_m_stap():
+    check_m_step()
+    check_m_step(use_MAP=True)
+
+
+def test_e_step_time():
+    check_e_step_time()
+    check_e_step_time(use_MAP=True)
+
+
+def test_m_step_time():
+    check_m_step_time()
+    check_m_step_time(use_MAP=True)
---
+++ hw/practice_1/utils/yusov.py
@@ -0,0 +1,238 @@
+import math
+
+import numpy as np
+
+
+def calculate_log_probability(X, F, B, s):
+    """
+    Calculates log p(X_k|d_k,F,B,s) for all images X_k in X and
+    all possible displacements d_k.
+
+    Parameters
+    ----------
+    X : array, shape (H, W, K)
+        K images of size H x W.
+    F : array, shape (h, w)
+        Estimate of villain's face.
+    B : array, shape (H, W)
+        Estimate of background.
+    s : float
+        Estimate of standard deviation of Gaussian noise.
+
+    Returns
+    -------
+    ll : array, shape(H-h+1, W-w+1, K)
+        ll[dh,dw,k] - log-likelihood of observing image X_k given
+        that the villain's face F is located at displacement (dh, dw)
+    """
+    H, W, K = X.shape
+    h, w = F.shape
+    dh = H - h + 1
+    dw = W - w + 1
+
+    ll = np.empty([dh, dw, K])
+
+    for i in range(dh):
+        for j in range(dw):
+            B_ = np.copy(B)  # copy is necessary
+            B_[i:i + h, j:j + w] = F
+            under_exponent = + (-1. / (2 * s ** 2)) * (X - np.expand_dims(B_, axis=-1)) ** 2
+            ll[i, j] = np.sum(under_exponent, axis=(0, 1))
+
+    ll -= H*W*(0.5 * math.log(2 * math.pi) + math.log(s))
+    return ll
+
+
+def calculate_lower_bound(X, F, B, s, A, q, use_MAP=False):
+    """
+    Calculates the lower bound L(q,F,B,s,A) for the marginal log likelihood.
+
+    Parameters
+    ----------
+    X : array, shape (H, W, K)
+        K images of size H x W.
+    F : array, shape (h, w)
+        Estimate of villain's face.
+    B : array, shape (H, W)
+        Estimate of background.
+    s : float
+        Estimate of standard deviation of Gaussian noise.
+    A : array, shape (H-h+1, W-w+1)
+        Estimate of prior on displacement of face in any image.
+    q : array
+        If use_MAP = False: shape (H-h+1, W-w+1, K)
+            q[dh,dw,k] - estimate of posterior of displacement (dh,dw)
+            of villain's face given image Xk
+        If use_MAP = True: shape (2, K)
+            q[0,k] - MAP estimates of dh for X_k
+            q[1,k] - MAP estimates of dw for X_k
+    use_MAP : bool, optional
+        If true then q is a MAP estimates of displacement (dh,dw) of
+        villain's face given image Xk.
+
+    Returns
+    -------
+    L : float
+        The lower bound L(q,F,B,s,A) for the marginal log likelihood.
+    """
+    pass
+
+
+def run_e_step(X, F, B, s, A, use_MAP=False):
+    """
+    Given the current esitmate of the parameters, for each image Xk
+    esitmates the probability p(d_k|X_k,F,B,s,A).
+
+    Parameters
+    ----------
+    X : array, shape(H, W, K)
+        K images of size H x W.
+    F  : array_like, shape(h, w)
+        Estimate of villain's face.
+    B : array shape(H, W)
+        Estimate of background.
+    s : scalar, shape(1, 1)
+        Eestimate of standard deviation of Gaussian noise.
+    A : array, shape(H-h+1, W-w+1)
+        Estimate of prior on displacement of face in any image.
+    use_MAP : bool, optional,
+        If true then q is a MAP estimates of displacement (dh,dw) of
+        villain's face given image Xk.
+
+    Returns
+    -------
+    q : array
+        If use_MAP = False: shape (H-h+1, W-w+1, K)
+            q[dh,dw,k] - estimate of posterior of displacement (dh,dw)
+            of villain's face given image Xk
+        If use_MAP = True: shape (2, K)
+            q[0,k] - MAP estimates of dh for X_k
+            q[1,k] - MAP estimates of dw for X_k
+    """
+    log_likelihood = calculate_log_probability(X, F, B, s)
+    log_prior = np.expand_dims(np.log(A), axis=-1)
+
+    # shape (H-h+1, W-w+1, K)
+    log_aposteriory_estimation = log_likelihood + log_prior
+    max_across_images = np.max(log_aposteriory_estimation, axis=(0, 1), keepdims=True)
+    log_aposteriory_exp = np.exp(log_aposteriory_estimation - max_across_images)
+
+    q = log_aposteriory_exp / np.sum(log_aposteriory_exp, axis=(0, 1), keepdims=True)
+
+    if not use_MAP:
+        return q
+    else:
+        raise NotImplementedError()
+
+
+def run_m_step(X, q, h, w, use_MAP=False):
+    """
+    Estimates F,B,s,A given esitmate of posteriors defined by q.
+
+    Parameters
+    ----------
+    X : array, shape(H, W, K)
+        K images of size H x W.
+    q  :
+        if use_MAP = False: array, shape (H-h+1, W-w+1, K)
+           q[dh,dw,k] - estimate of posterior of displacement (dh,dw)
+           of villain's face given image Xk
+        if use_MAP = True: array, shape (2, K)
+            q[0,k] - MAP estimates of dh for X_k
+            q[1,k] - MAP estimates of dw for X_k
+    h : int
+        Face mask height.
+    w : int
+        Face mask width.
+    use_MAP : bool, optional
+        If true then q is a MAP estimates of displacement (dh,dw) of
+        villain's face given image Xk.
+
+    Returns
+    -------
+    F : array, shape (h, w)
+        Estimate of villain's face.
+    B : array, shape (H, W)
+        Estimate of background.
+    s : float
+        Estimate of standard deviation of Gaussian noise.
+    A : array, shape (H-h+1, W-w+1)
+        Estimate of prior on displacement of face in any image.
+    """
+    pass
+
+
+def run_EM(X, h, w, F=None, B=None, s=None, A=None, tolerance=0.001,
+           max_iter=50, use_MAP=False):
+    """
+    Runs EM loop until the likelihood of observing X given current
+    estimate of parameters is idempotent as defined by a fixed
+    tolerance.
+
+    Parameters
+    ----------
+    X : array, shape (H, W, K)
+        K images of size H x W.
+    h : int
+        Face mask height.
+    w : int
+        Face mask width.
+    F : array, shape (h, w), optional
+        Initial estimate of villain's face.
+    B : array, shape (H, W), optional
+        Initial estimate of background.
+    s : float, optional
+        Initial estimate of standard deviation of Gaussian noise.
+    A : array, shape (H-h+1, W-w+1), optional
+        Initial estimate of prior on displacement of face in any image.
+    tolerance : float, optional
+        Parameter for stopping criterion.
+    max_iter  : int, optional
+        Maximum number of iterations.
+    use_MAP : bool, optional
+        If true then after E-step we take only MAP estimates of displacement
+        (dh,dw) of villain's face given image Xk.
+
+    Returns
+    -------
+    F, B, s, A : trained parameters.
+    LL : array, shape(number_of_iters,)
+        L(q,F,B,s,A) after each EM iteration (1 iteration = 1 e-step + 1 m-step); 
+        number_of_iters is actual number of iterations that was done.
+    """
+    pass
+
+
+def run_EM_with_restarts(X, h, w, tolerance=0.001, max_iter=50, use_MAP=False,
+                         n_restarts=10):
+    """
+    Restarts EM several times from different random initializations
+    and stores the best estimate of the parameters as measured by
+    the L(q,F,B,s,A).
+
+    Parameters
+    ----------
+    X : array, shape (H, W, K)
+        K images of size H x W.
+    h : int
+        Face mask height.
+    w : int
+        Face mask width.
+    tolerance, max_iter, use_MAP : optional parameters for EM.
+    n_restarts : int
+        Number of EM runs.
+
+    Returns
+    -------
+    F : array,  shape (h, w)
+        The best estimate of villain's face.
+    B : array, shape (H, W)
+        The best estimate of background.
+    s : float
+        The best estimate of standard deviation of Gaussian noise.
+    A : array, shape (H-h+1, W-w+1)
+        The best estimate of prior on displacement of face in any image.
+    L : float
+        The best L(q,F,B,s,A).
+    """
+    pass
